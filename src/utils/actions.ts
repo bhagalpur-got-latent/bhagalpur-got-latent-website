2d80cdf6e28ac2eb37fcbc55dce357ec092aaed8
bhagalpur-got-latent <bhagalpurgotlatent2025@gmail.com>
2025-02-03 08:38:12 +0000

    sixth commit

--- src/utils/actions.ts
+++ src/utils/actions.ts
@@ -7,14 +7,9 @@ const SCOPES = [
 ];
 const auth = new google.auth.GoogleAuth({
   credentials: {
-    type: "service_account",
-    project_id: process.env.GOOGLE_PROJECT_ID,
-    private_key: process.env.GOOGLE_PRIVATE_KEY,
     client_email: process.env.GOOGLE_CLIENT_EMAIL,
     client_id: process.env.GOOGLE_CLIENT_ID,
-    auth_uri: process.env.GOOGLE_AUTH_URI,
-    token_uri: process.env.GOOGLE_TOKEN_URI,
-    client_x509_cert_url: process.env.GOOGLE_CLIENT_X509_CERT_URL,
+    private_key: process.env.GOOGLE_PRIVATE_KEY?.replace(/\\n/g, "\n"), // Ensure proper formatting
   },
   scopes: SCOPES,
 });
